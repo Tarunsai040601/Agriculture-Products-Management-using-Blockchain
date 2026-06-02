04bd07ba7bd6a0c9fd6b0d1b62a3949ad7c21836
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-06-02 08:30:59 +0530

    added .js

--- Server/Routers/createDealer/Dealer.js
+++ Server/Routers/createDealer/Dealer.js
@@ -4,9 +4,9 @@ const {
   createDealer,
   deleteDealer,
   dealers,
-} = require("../../Controllers/DealerController/dealerController");
-const authMiddleware = require("../../Middlewares/AuthMiddleWares/authMiddleware");
-const roleMiddleware = require("../../Middlewares/RoleMiddleWares/roleMiddleware");
+} = require("../../Controllers/DealerController/dealerController.js");
+const authMiddleware = require("../../Middlewares/AuthMiddleWares/authMiddleware.js");
+const roleMiddleware = require("../../Middlewares/RoleMiddleWares/roleMiddleware.js");
 const dealerRoute = express.Router();
 
 // fetch the dealer
